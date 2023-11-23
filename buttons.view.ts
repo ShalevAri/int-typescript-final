1ddd0e9ef646b6bc81b779b8a46d276568a6b1d3
shalev <shalevari2008@gmail.com>
2023-11-23 18:37:23 +0200

    feat(references): add

--- buttons.view.ts
+++ buttons.view.ts
@@ -1,6 +1,9 @@
 export class View {
 	private confirmButton: HTMLButtonElement
 	private cancelButton: HTMLButtonElement
+	private recipientName: HTMLInputElement
+	private location: HTMLSelectElement
+	private time: HTMLSelectElement
 
 	constructor() {
 		this.confirmButton = document.querySelector(
@@ -9,6 +12,11 @@ export class View {
 		this.cancelButton = document.querySelector(
 			".cancel-button"
 		) as HTMLButtonElement
+		this.recipientName = document.querySelector(
+			".person-name"
+		) as HTMLInputElement
+		this.location = document.querySelector(".location") as HTMLSelectElement
+		this.time = document.querySelector(".time") as HTMLSelectElement
 	}
 
 	bindConfirmButton(callback: () => void): void {
@@ -20,6 +28,27 @@ export class View {
 
 	bindCancelButton(callback: () => void): void {
 		this.cancelButton.addEventListener("click", (e) => {
+			e.preventDefault()
+			callback()
+		})
+	}
+
+	bindRecipientName(callback: () => void): void {
+		this.recipientName.addEventListener("click", (e) => {
+			e.preventDefault()
+			callback()
+		})
+	}
+
+	bindLocation(callback: () => void): void {
+		this.location.addEventListener("select", (e) => {
+			e.preventDefault()
+			callback()
+		})
+	}
+
+	bindTime(callback: () => void): void {
+		this.time.addEventListener("select", (e) => {
 			e.preventDefault()
 			callback()
 		})
--- dist/buttons.view.js
+++ dist/buttons.view.js
@@ -5,6 +5,9 @@ var View = /** @class */ (function () {
     function View() {
         this.confirmButton = document.querySelector(".confirm-button");
         this.cancelButton = document.querySelector(".cancel-button");
+        this.recipientName = document.querySelector(".person-name");
+        this.location = document.querySelector(".location");
+        this.time = document.querySelector(".time");
     }
     View.prototype.bindConfirmButton = function (callback) {
         this.confirmButton.addEventListener("click", function (e) {
@@ -14,6 +17,24 @@ var View = /** @class */ (function () {
     };
     View.prototype.bindCancelButton = function (callback) {
         this.cancelButton.addEventListener("click", function (e) {
+            e.preventDefault();
+            callback();
+        });
+    };
+    View.prototype.bindRecipientName = function (callback) {
+        this.recipientName.addEventListener("click", function (e) {
+            e.preventDefault();
+            callback();
+        });
+    };
+    View.prototype.bindLocation = function (callback) {
+        this.location.addEventListener("select", function (e) {
+            e.preventDefault();
+            callback();
+        });
+    };
+    View.prototype.bindTime = function (callback) {
+        this.time.addEventListener("select", function (e) {
             e.preventDefault();
             callback();
         });
